ef374def72895add6abbebde73e4eed2e0f7ea59
Abele-n <otienoabele89@gmail.com>
2023-06-15 13:33:42 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
- *main - Entry point
- *Description: 'Program that prints a message followed by a new line'
- *Return: Always 0(success)
+ *main-Entry point
+ *Description:'Program that prints a message followed by a new line'
+ *Return:Always 0(success)
  */ 
 
 int main(void)
