7b8ab10d3e1346144c3431f1661ca1df5bdf6ed3
Abele-n <otienoabele89@gmail.com>
2023-06-15 13:37:57 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,12 @@
 /**
  *main-Entry point
  *Description:'Program that prints a message followed by a new line'
- *Return:Always 0(success)
+ *Return:Always 0 (success)
  */ 
 
 int main(void)
 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-	
 	return (0);
-
 }
