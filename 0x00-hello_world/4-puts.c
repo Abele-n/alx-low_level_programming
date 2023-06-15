b84d3248929ee2b43b7199cdcaadb09b2f8bac88
Abele-n <otienoabele89@gmail.com>
2023-06-15 13:14:22 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * return: always 0(success)
- *
+ *Description: 'Program that prints a message followed by a new line'
+ * return: Always 0(success)
  * / 
 
 int main(void)
 {
 
-        puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-        return (0);
+	return (0);
 
 }
