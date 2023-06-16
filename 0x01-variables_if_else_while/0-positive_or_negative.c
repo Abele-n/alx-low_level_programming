e45da7137572c59ae838a76bc967cb45bc99853d
Abele-n <otienoabele89@gmail.com>
2023-06-16 14:41:08 -0700

    task zero c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,40 @@
-#include<stdlib.h>
-#include<time.h>
-#include<stdio.h>
+*Full Stack Software Engineering studies** at **ALX School**. It aims to learn about arithmetic operators, relational operators, boolean operators, comments, declaring variables and loops in **C language**.
+
+* C files are written according to the C90 standard
+
+0-positive_or_negative.c
+
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
- *main-Determines if a number is positive, negative or zero.
- *Return:Always 0(Success)
+ * main - Determines if a number is positive, negative or zero.
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 
 {
-        int n;
+	int n;
+
 	srand(time(0));
-	n=rand() - RAND_MAX / 2;
+ 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
+		 printf("%d is positive\n", n);
+        }
+        else if (n == 0)
+        {
+                printf("%d is zero\n", n);
+        }
         else
         {
-                printf("%d is negative\n", n);
+
+                printf("%d is negative\n", n)
         }
+
         return (0);
 }
