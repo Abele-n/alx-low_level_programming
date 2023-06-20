9cc80e3ada29bea1ceb478659904f830d7535394
Abele-n <otienoabele89@gmail.com>
2023-06-20 13:19:59 -0700

    nested c functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add- adds two integers and prints the results
+ * @x: int to be added to y
+ * @y: int to be added to x
+ * return: sum of x and y
+ */
+int add(int x, int y)
+
+{
+	int c;
+
+	c = x + y;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs-computes the absolute value of an integer
+ * @n: int to check
+ * return: absolute value of int
+ */
+int abs(int n)
+
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
