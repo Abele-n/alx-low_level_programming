ccbd334967311a3fb05c78efc210dc1ecff955eb
Abele-n <otienoabele89@gmail.com>
2023-06-23 12:29:31 -0700

    Fizz-Buzz task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
@@ -34,8 +34,7 @@ int main(void)
 		{
 			printf(" %d", i);
 		}
-		printf("\n");
-		
+		printf("\n");		
 		return (0);
 	}
 }
