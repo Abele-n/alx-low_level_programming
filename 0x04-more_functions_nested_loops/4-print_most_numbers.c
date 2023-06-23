ac5fee3e7facff0f53705f9f2f774bea4f2e519a
Abele-n <otienoabele89@gmail.com>
2023-06-22 18:56:42 -0700

    task 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,7 +5,7 @@
  * print_most_numbers- print num except 2 and 4
  * return: void
  */
-void trim(char *c);
+
 void print_most_numbers(void)
 {
 	char c;
@@ -16,14 +16,4 @@ void print_most_numbers(void)
 			_putchar(c);
 	}
 	_putchar('\n');
-}
-void trim(char *c)
-{
-	int i = strlen(c) -1;
-	while (i > 0)
-	{
-		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t') i--;
-		else break;
-	}
-	s[i + 1] = '\0';
 }
