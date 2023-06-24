b854a8daaea746f59909a65bf19d41d746c1c2ed
Abele-n <otienoabele89@gmail.com>
2023-06-23 18:24:21 -0700

    The problem of distinguishing prime numbers from composite numbers

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,13 +10,13 @@
 int main(void)
 
 {
-	unsigned int i, n = 612852475143;
+	long int i, n = 612852475143;
 
 	for (i = 3; i <= 782849; i = i + 2)
 	{
 		while ((n % i == 0) && (n != i))
 			n = n / i;
 	}
-	printf("%ld\n", max);
+	printf("%ld\n", n);
 	return (0);
 }
