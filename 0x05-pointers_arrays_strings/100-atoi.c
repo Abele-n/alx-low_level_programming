154e40b04757a7efcaa15a011cede05637fe4052
Abele-n <otienoabele89@gmail.com>
2023-06-26 14:55:45 -0700

    task 10 array and pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,16 @@
 int _atoi(char *s)
 
 {
-	int i = 0; d = 0; n = 0; len = 0; f = 0; f = 0; digit = 0;
+	int i, d, n, len, f, digit;
+	i = 0;
+
+	d = 0;
+
+	n = 0;
+
+	len = 0;
+
+	f = 0;
 
 	digit = 0;
 
