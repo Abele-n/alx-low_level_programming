c1b1c9b757f3a7368e7c6e51786f27b3b79e0056
Abele-n <otienoabele89@gmail.com>
2023-06-29 04:32:35 -0700

    task 0.c strcat

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar- writes character c to stdout
+ * @c: character to print
+ * return: 1 on success
+ * on error return -1, and errno set appropriately
+ */
+int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
+}
