9a62237fbe65854282ee091e8c7b577f987cf6ab
Abele-n <otienoabele89@gmail.com>
2023-07-07 01:56:03 -0700

    0. It is not what they call you, it is what you answer to task 0.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: number of arguments supplied to the program
+ * @argv: array of pointers to the program
+ * Return: Always 0
+ */
+
 int main(int argc, char *argv[])
 {
 	int i;
