a9208c8c35cc41cfce764d8c79ac7e8d64a33aa7
Abele-n <otienoabele89@gmail.com>
2023-07-07 06:51:52 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _atoi - converts a string to an integer
@@ -11,17 +11,17 @@
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
-	
+
 	i = 0;
-	
+
 	d = 0;
-	
+
 	n = 0;
-	
+
 	len = 0;
-	
+
 	f = 0;
-	
+
 	digit = 0;
 
 	while (s[len] != '\0')
@@ -50,4 +50,30 @@ int _atoi(char *s)
 	if (f == 0)
 		return (0);
 	return (n);
+}
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = _atoi(argv[1]);
+
+	num2 = _atoi(argv[2]);
+
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
 }
