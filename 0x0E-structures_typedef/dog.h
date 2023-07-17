40d078692c46ef0406bb4ef6572d34705ac7f9de
Abele-n <otienoabele89@gmail.com>
2023-07-17 13:37:35 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - basic information
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: dog elements
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
