51ce080907233aaba4dc14d2980940bc36345e55
Abele-n <otienoabele89@gmail.com>
2023-07-27 14:09:29 -0700

    task 5.c The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void openning (void) __attribute__ ((constructor));
+void openning(void) __attribute__ ((constructor));
 
 /**
  * openning - prints the sentence just before the main is executed
