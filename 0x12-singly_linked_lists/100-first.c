d644c3e980784200e2861f4ab6b110a831a3b104
Abele-n <otienoabele89@gmail.com>
2023-07-27 14:04:58 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void openning (void) __attribute__ ((constructor));
+
+/**
+ * openning - prints the sentence just before the main is executed
+ */
+
+void openning(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,8 @@
+0x12. C - Singly linked lists
+
+Resources
+Read or watch:
+
+Linked Lists
+Google
+Youtube
