7ead51a55f886d4253dd2b5bddf046053865358e
Abele-n <otienoabele89@gmail.com>
2023-08-01 19:36:02 -0700

    task 11.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,10 +9,10 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t previous = NULL;
-	listint_t next = NULL;
+	listint_t previous;
+	listint_t next;
 
-	while (present)
+	while (*head)
 	{
 		listint_t *next_node = (*head)->next;
 		(*head)->next = previous;
