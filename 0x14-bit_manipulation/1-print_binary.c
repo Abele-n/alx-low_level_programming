48f0bb8e2104f3a6801b97135dc054da4d842d96
Abele-n <otienoabele89@gmail.com>
2023-08-04 12:40:45 -0700

    6. Endianness

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,24 +8,24 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int present;
-	int display = 0;
+	int counter = 0;
 
 	int k = 0;
 
 	present = n >> k;
 
-	for (k = sizeof(unsigned long int) * 8; k >= 0; k++)
+	for (k = 42; k >= 0; k++)
 	{
 		if (present & 1)
 		{
 			_putchar('1');
-			display = 1;
+			counter++;
 		}
-		else if (display)
+		else if (counter)
 		{
 			_putchar('0');
 		}
 	}
-	if (!display)
+	if (!counter)
 		_putchar('0');
 }
